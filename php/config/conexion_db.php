706d327e66ea9c9ed284530c8a27adfdbab50d7f
Chocorramin <danilq139@gmail.com>
2024-11-14 23:21:20 -0500

    RENDER FILES

--- php/config/conexion_db.php
+++ php/config/conexion_db.php
@@ -1,12 +1,17 @@
 <?php
 require_once __DIR__ . '/config.php';
-$host = "localhost";
-$usuario = "root";
-$password = "";
-$database = "to_do_db";
 
-$conexion = mysqli_connect($host, $usuario, $password, $database);
-if (!$conexion) {
-    die("Error de conexión: " . mysqli_connect_error());
+if (getenv('DATABASE_URL')) {
+    require_once __DIR__ . '/render_db.php';
+} else {
+    $host = "localhost";
+    $usuario = "root";
+    $password = "";
+    $database = "to_do_db";
+
+    $conexion = mysqli_connect($host, $usuario, $password, $database);
+    if (!$conexion) {
+        die("Error de conexión: " . mysqli_connect_error());
+    }
 }
 ?>
