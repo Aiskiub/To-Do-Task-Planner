7fa821d7e187fa08f00d700a86ed7c61c7563642
Chocorramin <danilq139@gmail.com>
2024-11-15 01:06:16 -0500

    Refactor: Simplify database connection setup
    Simplified database connection configuration by directly using environment variables provided by Render. Removed the conditional logic and the separate `render_db.php` file, streamlining the connection process and making it more efficient.  This also updates the Render configuration to use a Docker environment and fetch individual database credentials as separate environment variables.

--- php/config/conexion_db.php
+++ php/config/conexion_db.php
@@ -1,17 +1,14 @@
 <?php
 require_once __DIR__ . '/config.php';
 
-if (getenv('DATABASE_URL')) {
-    require_once __DIR__ . '/render_db.php';
-} else {
-    $host = "localhost";
-    $usuario = "root";
-    $password = "";
-    $database = "to_do_db";
+$host = getenv('MYSQL_HOST') ?: 'bklh49kulsoolhk6uayw-mysql.services.clever-cloud.com';
+$user = getenv('MYSQL_USER') ?: 'u4j93xe2z4ovjypf';
+$password = getenv('MYSQL_PASSWORD') ?: 'OhMPlDSPOQRaYaWVqE3s';
+$database = getenv('MYSQL_DATABASE') ?: 'bklh49kulsoolhk6uayw';
+$port = getenv('MYSQL_PORT') ?: '3306';
 
-    $conexion = mysqli_connect($host, $usuario, $password, $database);
-    if (!$conexion) {
-        die("Error de conexión: " . mysqli_connect_error());
-    }
+$conexion = mysqli_connect($host, $user, $password, $database, $port);
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
 }
 ?>
